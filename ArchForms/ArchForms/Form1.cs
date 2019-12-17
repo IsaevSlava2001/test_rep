cd2492b07b25ed8d72dc384beb2d60a00329a751
IsaevSlava2001 <IsaevSlava.2001@yandex.ru>
2019-12-18 00:04:14 +0300

    проверка2

--- ArchForms/ArchForms/Form1.cs
+++ ArchForms/ArchForms/Form1.cs
@@ -23,7 +23,7 @@ namespace ArchForms
             //Use use = new Use();
             //use.Battle();
             InitializeComponent();
-            // todo: потоковый читатель имени (провекрка)
+            // todo: потоковый читатель имени
             answerLabel.Text = "Ты будешь со мной играть?";
             button1.Text = "Да";
             button2.Text = "Нет";
